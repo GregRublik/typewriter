425515ef7e6f573b76544effbe1741974d032542
Иванов Григорий <grigorashekivanov2@gmail.com>
2026-08-26 17:35:26 +0300

    fix killed

--- src/services/ocr.py
+++ src/services/ocr.py
@@ -3,7 +3,13 @@ from paddleocr import PaddleOCR
 
 class OCRService:
     def __init__(self):
-        self.ocr = PaddleOCR(lang="ru", enable_mkldnn=False)
+        self.ocr = PaddleOCR(
+            lang="ru",
+            enable_mkldnn=False,
+            use_doc_orientation_classify=False,
+            use_doc_unwarping=False,
+            text_detection_model_name="PP-OCRv5_mobile_det",
+        )
 
     def run(self, document_path: str) -> str:
         result = self.ocr.predict(document_path)
